3187a1f2f383b8ebba7f517b875bbe2cce457ced
Maxim Polishchuck <mpolishchuck@gmail.com>
2013-12-03 01:47:02 +0200

    Made basic design

--- src/PaulMaxwell/GuestbookBundle/Controller/DefaultController.php
+++ src/PaulMaxwell/GuestbookBundle/Controller/DefaultController.php
@@ -2,12 +2,31 @@
 
 namespace PaulMaxwell\GuestbookBundle\Controller;
 
+use Doctrine\ORM\Query;
+use Knp\Component\Pager\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page = 1)
     {
-        return $this->render('PaulMaxwellGuestbookBundle:Default:index.html.twig');
+        $manager = $this->getDoctrine()->getManager();
+        /**
+         * @var Query $query
+         */
+        $query = $manager->createQuery('SELECT t FROM PaulMaxwellGuestbookBundle:Message t');
+
+        /**
+         * @var Paginator $paginator
+         */
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query,
+            $page
+        );
+
+        return $this->render('PaulMaxwellGuestbookBundle:Default:index.html.twig', array(
+            'posts' => $pagination,
+        ));
     }
 }
---
+++ src/PaulMaxwell/GuestbookBundle/Resources/translations/messages.ru.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    'Welcome to simple guestbook!' => 'Добро пожаловать в простую гостевую книгу!',
+    'Simple guestbook' => 'Простая гостевая книга',
+);
