94e02790ed511d402da39f3666baeaf0efa78b73
Maxim Polishchuck <mpolishchuck@gmail.com>
2013-12-11 21:48:07 +0200

    Added new post nitification via EventDispatcher

--- src/PaulMaxwell/GuestbookBundle/Resources/translations/messages.ru.php
+++ src/PaulMaxwell/GuestbookBundle/Resources/translations/messages.ru.php
@@ -7,4 +7,7 @@ return array(
     'Your message' => 'Ваше сообщение',
     'Fire!' => 'Отжечь!',
     'You can use letters only' => 'Можно использовать только буквы',
+    "New post added into guest book.\nSender: {name}\nTime: {time}\n\n{body}\n" => "В гостевую книгу было добавлено сообщение.\nОтправитель: {name}\nВремя: {time}\n\n{body}\n",
+    'New post added into guestbook from {name} on {time}' => 'В гостевую книгу было добавлено сообщение от {name} в {time}',
+    'New post notification' => 'Уведомление о новом сообщении',
 );
